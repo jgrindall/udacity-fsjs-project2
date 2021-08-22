55e744b837f3ba4b371362ad5bb80c8b39647447
jgrindall <john.grindall@gmail.com>
2021-08-22 14:40:16 +0100

    add api test

---
+++ spec/api/products_spec.ts
@@ -0,0 +1,48 @@
+import supertest from "supertest";
+import app from "../../src/server";
+import {Product} from "../../src/models/product";
+
+const request = supertest(app);
+
+describe("Test endpoint success", async () => {
+
+    it("test list", async () => {
+        const response = await request.get("/api/products");
+        expect(response.status).toBe(200);
+        expect(response.body).toEqual([]);
+    });
+
+    let idCreated: number;
+
+    it("test create", async () => {
+        const product:Omit<Product, "id"> = {
+            category:"home",
+            name:"name1",
+            price:10
+        };
+        const response = await request
+            .post("/api/products")
+            .send(product);
+        expect(response.status).toBe(200);
+        const product2:Product = (response.body as Product);
+        expect(product2).toBeTruthy();
+        expect(product2.name).toEqual(product.name);
+
+        idCreated = product2.id;
+
+        const response2 = await request.get("/api/products");
+        expect(response2.status).toBe(200);
+        const products = (response2.body as Product[]);
+        expect(products).toBeTruthy();
+        expect(products.length).toEqual(1);
+        expect(products[0].name).toEqual(product.name);
+    });
+
+    it("test get by id", async () => {
+        const response = await request.get("/api/products/" + idCreated);
+        expect(response.status).toBe(200);
+        expect(response.body.name).toEqual("name1");
+    });
+
+
+});
