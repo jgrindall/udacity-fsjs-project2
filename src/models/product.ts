485ceaf138f0e0d9f22746370d1e5dc2db1c36dc
jgrindall <john.grindall@gmail.com>
2021-08-22 12:38:31 +0100

    Add products model

---
+++ src/models/product.ts
@@ -0,0 +1,68 @@
+import client from "../database";
+
+export type Product = {
+    id:number;
+    name: string;
+    price: number;
+    category:string;
+}
+
+export class ProductStore{
+    constructor() {
+
+    }
+
+    async deleteAll(): Promise<Product[]> {
+        try {
+            const connection = await client.connect();
+            const sql = 'delete from products';
+            const result = await connection.query(sql);
+            connection.release();
+            return result.rows;
+        }
+        catch (err) {
+            throw new Error(`Could not del all products. Error: ${err}`)
+        }
+    }
+
+    async index(): Promise<Product[]> {
+        try {
+            const connection = await client.connect();
+            const sql = 'select * from products';
+            const result = await connection.query(sql);
+            connection.release();
+            return result.rows;
+        }
+        catch (err) {
+            throw new Error(`Could not get products. Error: ${err}`)
+        }
+    }
+
+    async find(id:number):Promise<Product>{
+        try{
+            const connection = await client.connect();
+            const sql = 'select * from products where id=$1';
+            const result = await connection.query(sql, [id]);
+            await connection.release();
+            return result.rows[0];
+        }
+        catch(e){
+            throw new Error("get products error " + e.message);
+        }
+    }
+
+    async create(name:string, price:number, category:string): Promise<Product> {
+        try {
+            const sql = 'insert into products (name, price, category) values($1, $2, $3) returning *';
+            const connection = await client.connect();
+            const result = await connection.query(sql, [name, price, category]);
+            await connection.release();
+            return result.rows[0];
+        }
+        catch (err) {
+            throw new Error(`Could not add new product. Error: ${err}`)
+        }
+    }
+
+}
+
