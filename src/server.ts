aedbd50782421eb6e61ad895fcab4f0e3894a393
jgrindall <john.grindall@gmail.com>
2021-08-22 12:48:37 +0100

    add product routing

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,28 @@
-import express, { Request, Response } from 'express'
+import express, { Request, Response, Application } from 'express'
 import bodyParser from 'body-parser'
+//import usersRoutes from "./handlers/api/users";
+//import orderRoutes from "./handlers/api/orders";
+import productRoutes from "./handlers/api/products";
+//import dashboardRoutes from "./handlers/api/dashboard";
 
-const app: express.Application = express()
-const address: string = "0.0.0.0:3000"
+const app: Application = express();
 
-app.use(bodyParser.json())
+const port: number = 3000;
+
+app.use(bodyParser.json());
+
+//app.use("/api/users/", usersRoutes);
+//app.use("/api/orders/", orderRoutes);
+app.use("/api/products/", productRoutes);
+//app.use("/api/dashboard/", dashboardRoutes);
 
 app.get('/', function (req: Request, res: Response) {
     res.send('Hello World!')
-})
+});
 
-app.listen(3000, function () {
-    console.log(`starting app on: ${address}`)
-})
+app.listen(port, function () {
+    console.log(`starting app on port: ${port}`)
+});
+
+export default app;
+
