0c62ac3b562c6f22c52afc72e4fee589ee547ee3
jgrindall <john.grindall@gmail.com>
2021-08-27 12:59:35 +0100

    add CORS

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
 import express, { Request, Response, Application } from 'express'
 import bodyParser from 'body-parser'
+import cors from "cors";
 import usersRoutes from "./handlers/api/users";
 import orderRoutes from "./handlers/api/orders";
 import productRoutes from "./handlers/api/products";
-//import dashboardRoutes from "./handlers/api/dashboard";
 
 const app: Application = express();
 
@@ -11,10 +11,15 @@ const port: number = 3000;
 
 app.use(bodyParser.json());
 
+// CORS-enable all end points
+app.use(cors({
+    origin: '*'
+}));
+
+// set up routing
 app.use("/api/users/", usersRoutes);
 app.use("/api/orders/", orderRoutes);
 app.use("/api/products/", productRoutes);
-//app.use("/api/dashboard/", dashboardRoutes);
 
 app.get('/', function (req: Request, res: Response) {
     res.send('Hello World!')
